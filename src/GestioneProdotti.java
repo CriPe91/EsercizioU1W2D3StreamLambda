d3a95933d2a140d57a2a12a4c155c6b9b6824ae5
CriPe91 <cristian.petta91@gmail.com>
2025-01-15 21:04:01 +0100

    EsercizioU1W2D3StreamLambda

--- src/GestioneProdotti.java
+++ src/GestioneProdotti.java
@@ -1,34 +1,149 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 public class GestioneProdotti {
+
+    private static List<Product> productList = new ArrayList<Product>();
+    private static List<Order> orderList = new ArrayList<Order>();
+    private static List<Customer> customerList = new ArrayList<Customer>();
+
+
     public static void main(String[] args) {
 
-        List<Prodotto> ListaProdotti = List.of(
-                new Prodotto("books",150.56),
-                new Prodotto("baby",120.30),
-                new Prodotto("boys",143.40),
-                new Prodotto("books",130.50),
-                new Prodotto("baby",132.54),
-                new Prodotto("boys",157.45)
-        );
+        createProductList();
+        createCustomerList();
+        createOrderList();
 
-        List<Ordine> ListaOrdini = List.of(
-                new Ordine((List<Prodotto>) ListaProdotti.get(0)),
-                new Ordine((List<Prodotto>) ListaProdotti.get(1)),
-                new Ordine((List<Prodotto>) ListaProdotti.get(2)),
-                new Ordine((List<Prodotto>) ListaProdotti.get(3)),
-                new Ordine((List<Prodotto>) ListaProdotti.get(4)),
-                new Ordine((List<Prodotto>) ListaProdotti.get(5))
-        );
+//        System.out.println("Products");
+//        productList.forEach(ele -> System.out.println(ele));
+//        System.out.println("Customer");
+//        customerList.forEach(System.out::println);
+//        System.out.println("Order");
+//        orderList.forEach(System.out::println);
 
-      List<Prodotto> PrimaLista = ListaProdotti.stream().filter(prodotto -> "books".equals(prodotto.category) && prodotto.price > 100).collect(Collectors.toList());
-      PrimaLista.forEach(prodotto -> System.out.println(prodotto.category + " " + prodotto.price));
 
-      List<Ordine> SecondaLista = ListaOrdini.stream().filter(ordine ->ordine.GetProdotti().stream().anyMatch(prodotto -> "baby".equals(prodotto.category)).collect(Collectors.toList());
+        // ESERCIZIO 1
+        System.out.println("Prodotti uguale a Books e maggiori di 100: ");
+        BooksFiltered().forEach(System.out::println);
+
+        // ESERCIZIO 2
+        System.out.println("Ordini con prodotti Baby: ");
+        BabyOrder().forEach(System.out::println);
+
+        // Esercizio 3
+        System.out.println("--- Esercizio 3 ---");
+        getBoys().forEach(System.out::println);
+
+        // Esercizio 4
+        System.out.println("--- Esercizio 4 ---");
+        getTierProducts().forEach(System.out::println);
+
+
+    }
+         // ESERCIZIO 1
+    public static List<Product> BooksFiltered(){
+           List<Product> listaFiltrata = productList.stream().filter(product -> product.getCategory().equals("Books") && product.getPrice()>100).toList();
+           return listaFiltrata;
     }
 
+        //ESERCIZIO 2
+    public static List<Order> BabyOrder(){
+        List<Order> ordineFiltrato = orderList.stream()
+                .filter(order -> order.getProducts()
+                        .stream().anyMatch(product -> product.getCategory().equals("Baby"))).toList();
+        return ordineFiltrato;
+    }
 
+    //ESERCIZIO 3
+
+    public static List<Product> getBoys() {
+        List<Product> listaBoys = productList.stream()
+                .filter(p -> p.getCategory().equals("Boys"))
+                .map(product -> {
+                    product.setPrice(product.getPrice() * 0.90);
+                    return product;
+                }).toList();
+        return listaBoys;
+    }
+
+    //ESERCIZIO 4
+
+    public static List<Product> getTierProducts() {
+        List<Order> filterOrder = orderList.stream()
+                .filter(order -> order.getCustomer().getTier() == 2
+                        && order.getOrderDate().isBefore(LocalDate.parse("2025-01-20"))
+                        && order.getOrderDate().isAfter(LocalDate.parse("2025-01-01")))
+                .toList();
+
+
+        List<Product> products = new ArrayList<>();
+        for (Order order : filterOrder) {
+            products.addAll(order.getProducts());
+        }
+        return products;
+    }
+
+        public static void createProductList(){
+
+        Product p1 = new Product(1,1000,"smartphone","iphone");
+     Product p2 = new Product(2, 127.15, "Books", "ABC");
+     Product p3 = new Product(3, 5.8, "Baby", "pannolini");
+     Product p4 = new Product(4, 31, "Books", "Il Signore Degli Anelli");
+     Product p5 = new Product(5, 100, "Boys", "Spiderman");
+     Product p6 = new Product(6, 2, "Baby", "Ciuccio");
+
+     productList.addAll(Arrays.asList(p1,p2,p3,p4,p5,p6));
+ }
+
+ public static void createCustomerList(){
+
+     Customer c1 = new Customer(1, "Aldo Baglio", 1);
+     Customer c2 = new Customer(2, "Giovanni Storti", 2);
+     Customer c3 = new Customer(3, "Giacomo Poretti", 3);
+     Customer c4 = new Customer(4, "Marina Massironi", 2);
+
+customerList.addAll(Arrays.asList(c1,c2,c3,c4));
+ }
+
+ public static void createOrderList(){
+
+     Order o1 = new Order(1, customerList.get(0));
+     Order o2 = new Order(2, customerList.get(1));
+     Order o3 = new Order(3, customerList.get(2));
+     Order o4 = new Order(4, customerList.get(3));
+     Order o5 = new Order(3, customerList.get(2));
+
+
+     Product p1 = productList.get(0);
+     Product p2 = productList.get(1);
+     Product p3 = productList.get(2);
+     Product p4 = productList.get(3);
+     Product p5 = productList.get(4);
+     Product p6 = productList.get(5);
+
+     o1.addProduct(p1);
+     o1.addProduct(p3);
+     o1.addProduct(p5);
+
+     o2.addProduct(p1);
+     o2.addProduct(p4);
+
+     o3.addProduct(p2);
+     o3.addProduct(p4);
+     o3.addProduct(p3);
+     o3.addProduct(p6);
+
+     o4.addProduct(p2);
+     o4.addProduct(p6);
+
+     o5.addProduct(p1);
+     o5.addProduct(p2);
+     o5.addProduct(p4);
+
+     orderList.addAll(Arrays.asList(o1,o2,o3,o4,o5));
+ }
 
 }
---
+++ src/Product.java
@@ -0,0 +1,57 @@
+
+public class Product {
+    private long id;
+    private String name;
+    private String category;
+    private double price;
+
+    public Product(long id, double price, String category, String name) {
+        this.id = id;
+        this.price = price;
+        this.category = category;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
+
